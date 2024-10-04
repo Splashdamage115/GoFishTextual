e95d475f49aa9664d4a2240748b27849a6b89682
DavidStrikaitis <124636547+Splashdamage115@users.noreply.github.com>
2024-10-04 13:59:19 +0100

    changed to use normal buttons, started work on confirm dialogue for computer draws

--- GoFishApp.py
+++ GoFishApp.py
@@ -72,30 +72,71 @@ def endGame():
 
 class AskForCard(Static):
     """The button of what card you would like to select"""
+    CSS = """
+    RadioButton {
+        cursor: none;
+    }
+    """
+
+    def redrawCards(self):
+        global player
+        for i, button in enumerate(self.buttons):
+            if i < len(player):
+                button.display = True
+                button.label = player[i]
+            else:
+                button.display = False
 
     def compose(self) -> ComposeResult:
+        global player
         """The most cards you can have is 13, so we make 13 buttons"""
-        with RadioSet(id="focus_me"):
-            yield RadioButton("1", id="first")
-            yield RadioButton("2", id="two"  )
-            yield RadioButton("3", id="three")
-            yield RadioButton("4", id="four" )
-            yield RadioButton("5", id="five" )
-            yield RadioButton("6", id="six"  )
-            yield RadioButton("7", id="seven")
-            yield RadioButton("8", id="eight")
-            yield RadioButton("9", id="nine" )
-            yield RadioButton("10", id="ten" )
-            yield RadioButton("11", id="eleven")
-            yield RadioButton("12", id="twelve")
-            yield RadioButton("13", id="thirteen")
+        self.radioSet = RadioSet(id="focus_me")
+        self.buttons = []
+        with self.radioSet:
+            for c in ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "nineth", "tenth", "eleventh", "twelth", "thirteenth"]:
+                 self.buttons.append(Button(c, id=c))
+                 yield self.buttons[-1]
         with Horizontal():
                 yield Label(id="index")
+        self.redrawCards()
+
+    def setParent(self,parent):
+        self.parentClass = parent
+
+    def on_button_pressed(self, event):
+            selected = 0
+            for i,n in enumerate(["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "nineth", "tenth", "eleventh", "twelth", "thirteenth"]):
+                if event.button.id == n:
+                    selected = i
+                    break
+
+            playerFetchCard(selected)
+            self.parentClass.remove_cardRequest()
+            self.radioSet._selected = None
     
 
-    
-    def on_radio_set_changed(self, event: RadioSet.Changed) -> None:
-        playerFetchCard(event.radio_set.pressed_index)
+
+class ConfirmDraw(Static):
+    """The Button of wether you have that card or not"""
+
+    def compose(self) -> ComposeResult:
+        """You either have the card or not"""
+        
+
+    def setParent(self,parent):
+        self.parentClass = parent
+
+    def on_button_pressed(self, event):
+            selected = 0
+            for i,n in enumerate(["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "nineth", "tenth", "eleventh", "twelth", "thirteenth"]):
+                if event.button.id == n:
+                    selected = i
+                    break
+
+            playerFetchCard(selected)
+            self.parentClass.remove_cardRequest()
+            self.radioSet._selected = None
+
 
 
 class GoFishApp(App):
@@ -107,15 +148,19 @@ class GoFishApp(App):
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
-        yield AskForCard()
+        self.cardRequest = AskForCard()
+        self.cardRequest.setParent(self)
+        yield self.cardRequest
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.dark = not self.dark
 
+    def remove_cardRequest(self):
+        self.cardRequest.redrawCards()
 
 
 if __name__ == "__main__":
     app = GoFishApp()
-    print(deck)
+    dealCards()
     app.run()
